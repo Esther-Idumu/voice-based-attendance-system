2029762a61ba94d422f7f67ab7ab226ba294bd17
Esther Idumu <Idumuesther3@gmail.com>
2026-04-18 01:40:49 +0100

    added database integration. created a file for schemas and another for models

--- app/main.py
+++ app/main.py
@@ -1,57 +1,67 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg2 
+from psycopg2.extras import RealDictCursor 
+from sqlalchemy.orm import Session
+import time 
+from . import models, schemas
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-class Student(BaseModel):
-    name: str
-    matric_number: str
+# Establish a connection to the database 
+while True:
+    try: 
+        conn = psycopg2.connect(host='localhost', database='voice_attendance_system', user='postgres', password='potatoes', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successful!")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error: ", error)
+        time.sleep(2)
 
-all_students = [{"name": "Esther Idumu", "matric_number": "BU22SEN1037", "id": 1},
-                {"name": "Darasimi Salau", "matric_number": "BU22SEN1002", "id": 2}]
-
-def find_student(id):
-    for student in all_students:
-        if student["id"] == id:
-            return student
-
-def find_index_student(id):
-    for i, student in enumerate(all_students):
-        if student['id'] == id:
-            return i
-
+# Get all the students from the database
 @app.get("/students")
-def get_students():
-    return{"data": all_students}
+def get_students(db: Session = Depends(get_db)):
+    students = db.query(models.Student).all()
+    return{"data": students}
 
+# Create a student in the database 
 @app.post("/students", status_code=status.HTTP_201_CREATED)
-def create_student(student: Student):
-    student_dict = student.dict()
-    student_dict['id'] = randrange(0, 100000)
-    all_students.append(student_dict)
-    return{"data": student_dict}
+def create_student(student: schemas.StudentBase, db: Session = Depends(get_db)):
+  new_student = models.Student(**student.dict())
+  db.add(new_student)
+  db.commit()
+  db.refresh(new_student)
+  
+  return{"data": new_student}
 
+# Get a student from the database 
 @app.get("/students/{id}")
-def get_student(id: int):
+def get_student(id: int, db: Session = Depends(get_db)):
 
-    student = find_student(id)
+    student = db.query(models.Student).filter(models.Student.id == id).first()
     if not student:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"student with id: {id} was not found")
     return {"student_detail": student}
 
-@app.put("/students/{id}")
-def update_student(id: int, student: Student):
-    index = find_index_student(id)
+# @app.put("/students/{id}")
+# def update_student(id: int, updated_student: schemas.StudentBase, db: Session = Depends(get_db)):
+#     student_query = db.query(models.Student).filter(models.Student.id == id)
+#     student = student_query.first()
 
-    if index == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"student with id: {id} does not exist")
+#     if student == None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+#                             detail=f"student with id: {id} does not exist")
 
-    student_dict = student.dict()
-    student_dict['id'] = id
-    all_students[index] = student_dict
-    return{"data": student_dict}
+#     student_query.update(update_student.dict(), synchronize_session=False)
+
+#     db.commit()
+#     return{"data": student_query.first()}
 
