d0abd6b9361a1af77c8886f0913cfbe138e88f2e
Esther Idumu <Idumuesther3@gmail.com>
2026-04-24 21:00:07 +0100

    added voice embedding functionality using fake embeddings

--- app/models.py
+++ app/models.py
@@ -1,13 +1,28 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, JSON, DateTime
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 from .database import Base
 
 class Student(Base):
     __tablename__ = "students"
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     matric_number = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    embeddings = relationship(
+        "VoiceEmbedding", 
+        back_populates="student",
+        cascade="all, delete")
+
+class VoiceEmbedding(Base):
+    __tablename__ = "voice_embeddings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    student_id = Column(Integer, ForeignKey("students.id"))
+    embedding = Column(JSON, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    student = relationship("Student", back_populates="embeddings")
